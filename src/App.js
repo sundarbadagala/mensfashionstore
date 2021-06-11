ba0156d62d9e325a8b7dbab2632d5d636ef2002c
unknown <sundar.badagala@gmail.com>
2021-06-11 16:30:11 +0530

    'started-the-project'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar/Navbar'
+import Products from './components/Products/Products'
+import Cart from './components/Cart/Cart'
+import {ProductProvider} from './components/contextAPI'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductProvider>
+    <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/'><Products/></Route>
+          <Route path='/cart'><Cart/></Route>
+        </Switch>
+    </Router>
+    </ProductProvider>
   );
 }
 
